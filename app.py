189a4a5612229267fa612d6c2f8e4b82d8f5e8e2
YaKuLzA-bit <mnklinces@gmail.com>
2026-07-02 18:34:33 -0300

    Refactor loan simulation code in app.py
    Fix indentation and improve variable names in loan simulation.

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import streamlit as st
 st.title("Banco Central Da Sua Maquinna De Churros")
 st.write('texta ai se funciona e depois coloca frente e verso do cartao')
 casa = st.number_input('qual valor do cafofo q vc vai financiar:', min_value=0.0)
-salario = st.number_input('fala ai pa nois quanto vc rece:', min_value=0.0)
-anos = st.number_input('quantos anos vai pagar o barraco?', min_value=1)
+salario = st.number_input('fala ai pa nois quanto vc recebe:', min_value=0.0)
+anos = st.number_input('quantos anos vai pagar o barraco:', min_value=1)
 
 if st.button('Simular empréstimo'):
     meses = anos * 12
